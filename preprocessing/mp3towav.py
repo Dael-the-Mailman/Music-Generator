88df19ab01b7bf5d1ebceaa739728a1574c0c3e1
Dael-the-Mailman <kalebugalde@gmail.com>
2021-01-02 22:56:47 -0800

    Started work on .wav to spectrogram program

--- preprocessing/mp3towav.py
+++ preprocessing/mp3towav.py
@@ -1,12 +1,14 @@
 import os
 import re
 import pydub
+
 from pydub import AudioSegment
+from tqdm import tqdm
 
 IN_PATH = "E:/datasets/youtube/audiofiles"
 OUT_PATH = "E:/datasets/youtube/wavfiles"
  
-for song in os.listdir(IN_PATH):
+for song in tqdm(os.listdir(IN_PATH)):
     path_to_song = os.path.join(IN_PATH, song)
     sound = AudioSegment.from_mp3(path_to_song)
     filename = os.path.basename(path_to_song)
