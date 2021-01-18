f59f912f00354f87658b4cbfe3863e8974bf775a
Dael-the-Mailman <kalebugalde@gmail.com>
2021-01-18 11:57:50 -0800

    Data can only be loaded in batch sizes of 1

--- test/modules/test_datamodule.py
+++ test/modules/test_datamodule.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append('../../')
 from modules.datamodule import *
+from tqdm import tqdm
 
 train_set = TrainDataset('E:/datasets/youtube/wavfiles')
 valid_set = ValidDataset('E:/datasets/youtube/wavfiles')
@@ -13,3 +14,10 @@ print(len(test_set))
 print(train_set[0][0].shape)
 print(valid_set[0][0].shape)
 print(test_set[0][0].shape)
+train_dl = DataLoader(train_set, batch_size=1)
+num_iterations = len(train_set)
+for i, (src, trg) in tqdm(enumerate(train_dl)):
+    if (i+1) % 10 == 0:
+        print(f"Step: {i+1}/{num_iterations}, \
+                inputs: {src.squeeze(0).shape}, \
+                outputs: {trg.squeeze(0).shape}")
