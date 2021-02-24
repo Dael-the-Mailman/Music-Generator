99515583b207faee20bccbba77db9745adb66fa1
Dael-the-Mailman <kalebugalde@gmail.com>
2021-02-24 00:14:11 -0800

    Fixed memory allocation issue

--- train_gan.py
+++ train_gan.py
@@ -1,13 +1,105 @@
-from wgan_modules.wgan_gp import WGAN_GP
-from modules.datamodule import LofiDataModule
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import optuna
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.cuda.amp as amp
 
-seed_everything(42)
-dm = LofiDataModule('E:/datasets/youtube/wavfiles')
-model = WGAN_GP()
-trainer = Trainer(gpus=-1, auto_select_gpus=True, precision=16,
-                  deterministic=True, val_check_interval=0.5,
-                  automatic_optimization=False, overfit_batches=0.1,
-                  callbacks=[EarlyStopping(monitor='crit_val_loss')])
-trainer.fit(model, dm)
+from tqdm import tqdm
+from wgan_modules.critic import Critic
+from wgan_modules.generator import Generator
+from modules.datamodule import TrainDataset
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# Hyperparameters
+device = "cuda" if torch.cuda.is_available() else "cpu"
+LEARNING_RATE = 1e-4
+BATCH_SIZE = 64
+Z_DIM = 100
+AUDIO_CHANNELS = 2
+NUM_EPOCHS = 100
+FEATURES_CRITIC = 16
+FEATURES_GEN = 16
+CRITIC_ITERATIONS = 5
+LAMBDA_GP = 10
+
+loader = DataLoader(TrainDataset('E:/datasets/youtube/wavfiles'), shuffle=True)
+
+gen = Generator(Z_DIM, AUDIO_CHANNELS, FEATURES_GEN).to(device)
+crit = Critic(AUDIO_CHANNELS, FEATURES_CRITIC).to(device)
+
+opt_g = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+opt_c = optim.Adam(crit.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+scaler = amp.GradScaler()
+
+fixed_noise = torch.randn(64, Z_DIM, 1).to(device)
+# writer_real = SummaryWriter(f"logs/AudioGAN/real")
+# writer_fake = SummaryWriter(f"logs/AudioGAN/fake")
+step = 0
+
+gen.train()
+crit.train()
+
+def gradient_penalty(critic, real, fake, device="cpu"):
+    with amp.autocast():
+        BATCH_SIZE, C, S = real.shape
+        alpha = torch.rand((BATCH_SIZE, 1, 1)).repeat(1, C, S).to(device)
+        interpolated_images = real * alpha + fake * (1 - alpha)
+
+        # Calculate critic scores
+        mixed_scores = critic(interpolated_images)
+
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=scaler.scale(mixed_scores),
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    with amp.autocast():
+        gradient = gradient.view(gradient.shape[0], -1)
+        gradient_norm = gradient.norm(2, dim=1)
+        gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (song, _) in tqdm(enumerate(loader)):
+        song = song.squeeze(0).to(device)
+        song = torch.split(song, BATCH_SIZE) # Splits the song to decrease memory usage
+        
+        for real in song:
+            curr_batch_size = real.shape[0]
+
+            # Train Critic: max E[critic(real)] - E[critic(fake)]
+            # equivalent to minimizing the negative of that
+            for _ in range(CRITIC_ITERATIONS):
+                with amp.autocast():
+                    noise = torch.randn(curr_batch_size, Z_DIM, 1).to(device)
+                    fake = gen(noise)
+                    crit_real = crit(real).reshape(-1)
+                    crit_fake = crit(fake).reshape(-1)
+                    gp = gradient_penalty(crit, real, fake, device=device)
+                    loss_crit = -(
+                        torch.mean(crit_real) - torch.mean(crit_fake) + LAMBDA_GP * gp
+                    )
+                crit.zero_grad()
+                scaler.scale(loss_crit).backward(retain_graph=True)
+                scaler.step(opt_c)
+                scaler.update()
+
+            # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
+            with amp.autocast():
+                gen_fake = crit(fake).reshape(-1)
+                loss_gen = -torch.mean(gen_fake)
+            gen.zero_grad()
+            scaler.scale(loss_gen).backward()
+            scaler.step(opt_g)
+            scaler.update()
+
+        if batch_idx % 100 == 0 and batch_idx > 0:
+            print(
+                f"Epoch [{epoch + 1}/{NUM_EPOCHS}] Batch {batch_idx}/{len(loader)} \
+                Loss D: {loss_crit:.4f}, loss G: {loss_gen:.4f}"
+            )
+    break
--- wgan_modules/critic.py
+++ wgan_modules/critic.py
@@ -5,10 +5,35 @@ Based off code from https://github.com/aladdinpersson/Machine-Learning-Collectio
 """
 import torch
 import torch.nn as nn
+import torch.cuda.amp as amp
 
-class Critic(nn.Sequential):
+# class Critic(nn.Sequential):
+#     def __init__(self, channels, features_d):
+#         super().__init__(
+#             # Input: Seconds x channels x 44100
+#             nn.Conv1d(channels, features_d, kernel_size=10, stride=10, padding=0),
+#             # Blocks below turn 4410 -> 3
+#             self._block(features_d, features_d * 2, 10, 10), # 441
+#             self._block(features_d * 2, features_d * 4, 7, 7), # 63
+#             self._block(features_d * 4, features_d * 8, 7, 7), # 9
+#             self._block(features_d * 8, features_d * 16, 3, 3), # 3
+#             # Turns 3 into 1
+#             nn.Conv1d(features_d*16, 1, kernel_size=3, stride=1, padding=0)
+#         )
+    
+#     def _block(self, in_channels, out_channels, kernel_size, stride, padding=0):
+#         return nn.Sequential(
+#             nn.Conv1d(
+#                 in_channels, out_channels, kernel_size, stride, padding, bias=False,
+#             ),
+#             nn.InstanceNorm1d(out_channels, affine=True),
+#             nn.GELU(),
+#         )
+
+class Critic(nn.Module):
     def __init__(self, channels, features_d):
-        super().__init__(
+        super().__init__()
+        self.crit = nn.Sequential(
             # Input: Seconds x channels x 44100
             nn.Conv1d(channels, features_d, kernel_size=10, stride=10, padding=0),
             # Blocks below turn 4410 -> 3
@@ -19,6 +44,10 @@ class Critic(nn.Sequential):
             # Turns 3 into 1
             nn.Conv1d(features_d*16, 1, kernel_size=3, stride=1, padding=0)
         )
+
+    def forward(self, x):
+        with amp.autocast():
+            return self.crit(x)
     
     def _block(self, in_channels, out_channels, kernel_size, stride, padding=0):
         return nn.Sequential(
